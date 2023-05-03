4478ad7f228a22b880ca080a054aaae2a89f6e0c
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-03 14:22:56 +0200

    NixOS/nixpkgs#227071 test: use playwright-test

--- shell.nix
+++ shell.nix
@@ -1,8 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [
-      pkgs.nodejs
-    ];
-}
+let
+  nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/9665f56e3b9c0bfaa07ca88b03b62bb277678a23.tar.gz";
+  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
+in
+with pkgs;
+mkShell {
+  buildInputs = [
+    nodejs
+    playwright-test
+  ];
 
+  PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}";
+
+  shellHook = ''
+    # Remove playwright from node_modules, so it will be taken from playwright-test
+    rm node_modules/@playwright/ -R
+  '';
+}
